40a1397d3d177f55be4b8177ec0e37c135531a7e
Michael Burney <burney318@gmail.com>
2016-02-20 15:37:22 -0600

    Addition and update to Index.html/simon.css/simon.js

---
+++ public/simon.js
@@ -0,0 +1,12 @@
+"use strict";
+document.write("test external js")
+console.log("test external js")
+
+
+// variable list
+var green = 0;
+var red = 1;
+var yellow = 2;
+var blue = 3;
+
+var rNumber = Math.floor(Math.random()*4);
